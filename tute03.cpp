f3e3faa5f542e117cf009840d5aa2ce50e09191a
IT21290756 <99703916+IT21290756@users.noreply.github.com>
2022-02-22 03:26:11 +0000

    Completed

--- tute03.cpp
+++ tute03.cpp
@@ -4,20 +4,57 @@ Convert the C program given below which calculates the Factorial of a number tha
 
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+[23:43, 20/02/2022] Dilun 5.1 S2: #include<iostream>
+using namespace std;
+
 int main()
 {
-    int no;
-    long fac;
+  int no;
+  long fac;
 
-    printf("Enter a Number : ");
-    scanf("%d", &no);
+  cout << "Enter a Number : ";
+  cin >> no;
 
-    fac = 1;
-    for (int r=no; r >= 1; r--) {
-        fac = fac * r;
-    }
+  fac = 1;
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * r;
+  }
+  
+  cout << "Factorial of " << no << " is " << fac << endl;
 
-    printf("Factorial of %d is %ld\n", no, fac);  
-    return 0;
+  return 0;
+}
+[23:43, 20/02/2022] Dilun 5.1 S2: 3
+[23:43, 20/02/2022] Dilun 5.1 S2: #include<iostream>
+
+long Factorial(int no);
+long nCr(int n, int r);
+
+int main() {
+  int n, r;
+
+  std::cout << "Enter a value for n ";
+  std::cin >> n;
+  std::cout << "Enter a value for r ";
+  std::cin >> r;
+  std::cout << "nCr = ";
+  std::cout << nCr(n,r);
+  std::cout << std::endl;
+
+}
+
+long Factorial(int no)
+{
+  int fac = 1;
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * r;
+  }
+  return fac;
+}
+
+long nCr(int n, int r)
+{
+  return Factorial(n) / (Factorial(r) * Factorial(n-r));
 }
--- tute04.cpp
+++ tute04.cpp
@@ -11,13 +11,14 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+#include<iostream>
 
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
   int n, r;
+
   std::cout << "Enter a value for n ";
   std::cin >> n;
   std::cout << "Enter a value for r ";
@@ -25,5 +26,20 @@ int main() {
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
-  return 0;
+
+}
+
+long Factorial(int no)
+{
+  int fac = 1;
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * r;
+  }
+  return fac;
+}
+
+long nCr(int n, int r)
+{
+  return Factorial(n) / (Factorial(r) * Factorial(n-r));
 }
